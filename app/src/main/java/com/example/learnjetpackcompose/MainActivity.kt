4bf67fccae949725ae586483f67681d99b414152
pawanroy <proy@teckborn.com>
2022-07-12 16:52:37 +0530

    Conversation

--- app/src/main/java/com/example/learnjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/learnjetpackcompose/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.learnjetpackcompose
 
-import android.content.res.Configuration
+// ...
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -13,11 +14,10 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.learnjetpackcompose.data.SampleData
 import com.example.learnjetpackcompose.ui.theme.LearnJetpackComposeTheme
@@ -27,47 +27,60 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LearnJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                Conversation(SampleData.conversationSample)
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages){message ->
+            MessageCard(message)
+        }
+    }
 }
 
-data class Message(val message: String, val author: String)
+data class Message(val body: String, val author: String)
 
 @Composable
-fun MessageCard(message: Message) {
+fun MessageCard(msg: Message) {
     Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(R.drawable.profile_picture),
-            contentDescription = "Just an image",
+            contentDescription = null,
             modifier = Modifier
                 .size(40.dp)
-                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondaryVariant, CircleShape)
         )
         Spacer(modifier = Modifier.width(8.dp))
-        Column {
+
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded: Boolean by remember { mutableStateOf(false) }
+
+        // We toggle the isExpanded variable when we click on this Column
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
-                message.author,
-                style = MaterialTheme.typography.subtitle2,
-                color = MaterialTheme.colors.secondary
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
             )
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+            ) {
                 Text(
-                    message.message,
+                    text = msg.body,
                     modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.body2
                 )
             }
@@ -75,35 +88,3 @@ fun MessageCard(message: Message) {
     }
 }
 
-@Preview(showBackground = true)
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true, name = "Night Mode")
-@Composable
-fun MessageCardPreview() {
-    LearnJetpackComposeTheme {
-        MessageCard(message = Message("Hii There!", "Pawan Roy"))
-    }
-}
-
-@Preview(showBackground = true, name = "Day Mode")
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true, name = "Night Mode")
-@Composable
-fun DefaultPreview() {
-    LearnJetpackComposeTheme {
-        Greeting("Android")
-    }
-}
-
-@Composable
-fun Conversation(messages: List<Message>) {
-    LazyColumn {
-        items(messages) { message ->
-            MessageCard(message)
-        }
-    }
-}
-
-@Preview
-@Composable
-fun PreviewConversation(){
-    Conversation(messages = SampleData.conversationSample)
-}
